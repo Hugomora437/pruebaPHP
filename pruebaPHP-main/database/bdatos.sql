7c1cfbfb722e8216d9ca4dc682cd786e63a5c564
hugofabianmora <hugofabianmora2020@gmail.com>
2025-08-29 20:33:59 -0500

    Actualizacion base de datos

--- pruebaPHP-main/database/bdatos.sql
+++ pruebaPHP-main/database/bdatos.sql
@@ -15,3 +15,27 @@ CREATE TABLE IF NOT EXISTS t_usuarios (
 )ENGINE=InnoDB;
 
 INSERT INTO t_usuarios VALUE(NULL, 'HUGO','MORA','HUGOMORA1437@GMAIL.COM','14372020','admin','imagen');
+
+CREATE TABLE IF NOT EXISTS t_categorias (
+    id_categoria INT(11)      AUTO_INCREMENT    NOT NULL,
+    nombre       VARCHAR(100)                   NOT NULL,
+    CONSTRAINT pk_categoria   PRIMARY KEY(id_categoria)
+)ENGINE=InnoDB;
+
+INSERT INTO t_categorias VALUE(NULL, 'Construccion'),(NULL, 'electricos'),(NULL, 'Pinturas'),(NULL, 'pisos');
+
+
+CREATE TABLE IF NOT EXISTS t_productos (
+    id_producto INT(11)      AUTO_INCREMENT    NOT NULL,
+    nombre_producto     VARCHAR(100)           NOT NULL,
+    descripcion         VARCHAR(200)           NOT NULL,
+    stock               INT(100)               NOT NULL,
+    precio              FLOAT(10,9)            NOT NULL,
+    imagen              VARCHAR(255)           NOT NULL,
+    oferta              VARCHAR(200)           NOT NULL,
+    fecha               DATE                   NOT NULL,
+    id_categoria        INT(11)                NOT NULL,
+    CONSTRAINT pk_producto PRIMARY KEY(id_producto),
+    CONSTRAINT fk_categoria FOREIGN KEY(id_categoria) REFERENCES t_categorias(id_categoria)
+
+)ENGINE=InnoDB;
